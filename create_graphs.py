b2975c01542e60f97dad82281e9c8fee38b60195
Adel Bajcsi <bajcsiadel@yahoo.com>
2019-02-26 11:09:00 +0200

    AB: coloring added to craph generation

--- create_graphs.py
+++ create_graphs.py
@@ -8,6 +8,11 @@ from os import listdir
 from os.path import isfile, join
 from datetime import datetime
 
+import colorama
+from colorama import Fore, Style
+
+colorama.init(autoreset=True)
+
 regex_pattern = ".*"
 if len(sys.argv) == 2:
     regex_pattern = sys.argv[1]
@@ -17,36 +22,39 @@ files = [f for f in listdir(path) if re.match(regex_pattern, f)]
 files.sort()
 pp.pprint(files)
 
-plt.figure(figsize=(10, 20))
+if len(files) != 0:
+    plt.figure(figsize=(10, 20))
 
-graph_path = './results/graphs/'
-for i, f in enumerate(files):
-    t = []
-    x = []
-    y = []
-    with open(join(path, f), 'r') as fd:
-        for line in fd:
-            l = line.split()
-            t.append(int(l[0]))
-            x.append(float(l[1]))
-            y.append(float(l[2]))
+    graph_path = './results/graphs/'
+    for i, f in enumerate(files):
+        t = []
+        x = []
+        y = []
+        with open(join(path, f), 'r') as fd:
+            for line in fd:
+                l = line.split()
+                t.append(int(l[0]))
+                x.append(float(l[1]))
+                y.append(float(l[2]))
 
-        r = [np.sqrt(x[i]*x[i] + y[i]*y[i]) for i in range(len(x))]
+            r = [np.sqrt(x[i]*x[i] + y[i]*y[i]) for i in range(len(x))]
 
-        plt.title(f)
-        # plt.plot(t, r, label=f.split('.')[0])
-        plt.plot(t, x, label='x')
-        plt.plot(t, y, label='y')
+            plt.title(f)
+            plt.plot(t, r, label=f.split('.')[0])
+            # plt.plot(t, x, label='x')
+            # plt.plot(t, y, label='y')
 
-lgd = plt.legend()
-image_name = 'distances.png'
-image_path = join(graph_path, image_name)
-if isfile(image_path):
-    now = datetime.now()
-    [ name, extension ] = image_name.split('.')
-    image_name = name + '_' + now.strftime("%Y%m%d_%H%M%S") + "." + extension
+    lgd = plt.legend()
+    image_name = 'distances.png'
     image_path = join(graph_path, image_name)
+    if isfile(image_path):
+        now = datetime.now()
+        [ name, extension ] = image_name.split('.')
+        image_name = name + '_' + now.strftime("%Y%m%d_%H%M%S") + "." + extension
+        image_path = join(graph_path, image_name)
 
-plt.savefig(image_path, bbox_extra_artists=(lgd,), bbox_inches='tight')
-print("\033[1;32mSaved to file " + image_path + "\n")
-plt.cla()
+    plt.savefig(image_path, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    print(Fore.GREEN + Style.BRIGHT + "Saved to file " + image_path + "\n")
+    plt.cla()
+else:
+    print(Fore.RED + Style.BRIGHT + "ERROR (create_graph.py: line 60)\n\tNo file selected!\n")
