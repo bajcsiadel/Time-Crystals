f926da410d959a3cd9ae10a1a44d74ab4a17a694
Adel Bajcsi <bajcsiadel@yahoo.com>
2019-02-26 11:09:34 +0200

    AB: new config file created

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd ./source
 make
 cd ../
 
-for param in `ls -a ./parameters | egrep '^[0-9]+ero.json$'`
+for param in `ls -a ./parameters | egrep '^.*.json$'`
 do
     NAME=`echo $param | cut -d'.' -f1`
     DATE=`date +%Y%m%d_%H%M%S`
